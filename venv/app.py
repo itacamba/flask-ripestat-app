44b8f45353dcd07cbeca09a4e67f6dc2e053fda8
itacamba <daniela.sahagun03@gmail.com>
2020-12-15 13:56:25 -0500

    created regex for inetnum and organisation objects

--- venv/app.py
+++ venv/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import requests
 import pprint
 
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    r = requests.get('https://stat.ripe.net/data/whois/data.json?resource=185.83.156.0')
-    data = r.json()['data']['records'][0]
-    return render_template('ripe_object.html', data=data)
+# @app.route('/')
+# def index():
+#     r = requests.get('https://stat.ripe.net/data/whois/data.json?resource=185.83.156.0')
+#     data = r.json()['data']['records'][0]
+#     return render_template('ripe_object.html', data=data)
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        obj = request.form['ripe_object']
+        if re.match(r'^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$', obj):
+            return redirect(url_for('ripe_object', obj=obj))
+        else:
+            return "no match"
+    else:
+        return render_template('home.html', title='Home Page')
+
+
+@app.route('/<obj>')
+def ripe_object(obj):
+    return render_template('ripe_object.html', obj=obj, title="Ripe Object")
+    # return render_template('ripe_object.html', title="Ripe Object", obj=obj)
